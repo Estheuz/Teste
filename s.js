610bab1ce9be92b7b5070f0adacc476ebc7049b2
Estheuz <luumcletaja@gmail.com>
2022-10-25 20:10:23 -0300

    É isso aí

---
+++ s.js
@@ -0,0 +1,11 @@
+
+function mostrar (){
+    let checkPoema = document.getElementsByName("poema")
+    for (let i = [0]; i <checkPoema.length; i++) {
+        if (checkPoema[i].checked == true){
+        
+        }
+    }
+}
+
+
